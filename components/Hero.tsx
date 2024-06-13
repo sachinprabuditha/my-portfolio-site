2066fa91ab0280f394670d7ff8a8dadd79bef43e
Sachin Prabuditha <sachinprabuditha@gmail.com>
2024-06-14 00:54:29 +0530

    feat: Add lazy loading for images and improve Hero component
    - Add lazy loading for images in the `layout.tsx` file to improve page load performance.
    - Update the `Hero.tsx` component to include a new effect for generating animated text.
    - Create a new file `TextGenerateEffect.tsx` in the `components/ui` directory to implement the text generation effect.
    
    ```

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Spotlight } from './ui/Spotlight'
+import { TextGenerateEffect } from './ui/TextGenerateEffect'
 
 
 const Hero = () => {
@@ -16,12 +17,23 @@ const Hero = () => {
 
       </div>
 
-      <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.3] bg-grid-black/[0.2] relative flex items-center justify-center">
+      <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.03] bg-grid-black-100/[0.2] absolute top-0 left-0 flex items-center justify-center">
       {/* Radial gradient for the container to give a faded look */}
-      <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"/></div>
-      
-    
+      <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"/>
+      </div>
 
+      <div className='flex justify-center relative my-20 z-10'>
+        <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
+          <p className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-100">Modern & Minimalist JS Mastery Portfolio</p>
+
+          <TextGenerateEffect words="Transforming Concepts into Seamless User Experiences" className="text-center text-[40px] md:text-5xl lg:text-6xl"/>
+
+          <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
+            Hi! I&apos;m Sachin, a Next.js Developer based in Sri Lanka.
+          </p>
+        </div>
+
+      </div>
 
     </div>
   )
