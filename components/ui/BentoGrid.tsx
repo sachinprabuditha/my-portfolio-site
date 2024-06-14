4111c160c347ca007c4bd41488fd1e1a1057e5c5
Sachin Prabuditha <sachinprabuditha@gmail.com>
2024-06-15 03:01:41 +0530

    feat: Add @react-three/drei and @react-three/fiber dependencies
    This commit adds the "@react-three/drei" and "@react-three/fiber" dependencies to the project. These dependencies are required for implementing 3D graphics and effects in the application.

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -1,6 +1,13 @@
 import { cn } from "@/utils/cn";
 import React from "react";
 import { BackgroundGradientAnimation } from "./GradientBg";
+import { GlobeDemo } from "./GridGlobe";
+import MagicButton from "./MagicButton";
+import { IoCopyOutline } from "react-icons/io5";
+import { useState } from "react";
+import Lottie from "react-lottie";
+
+
 
 export const BentoGrid = ({
   className,
@@ -58,7 +65,7 @@ export const BentoGridItem = ({
           "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
       }}
     >
-        <div className={`${id === 6 && "flex justify-center"} h-full`}>
+        <div className={`${id === 6} && flex justify-center h-full`}>
         <div className="w-full h-full absolute">
           {img && (
             <img
@@ -89,17 +96,33 @@ export const BentoGridItem = ({
           </BackgroundGradientAnimation>
         )}
 
-      {header}
-      <div className="group-hover/bento:translate-x-2 transition duration-200">
-        {icon}
-        <div className="font-sans font-bold text-neutral-600 dark:text-neutral-200 mb-2 mt-2">
-          {title}
+<div
+          className={cn(
+            titleClassName,
+            "group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10"
+          )}
+        >
+          {/* change the order of the title and des, font-extralight, remove text-xs text-neutral-600 dark:text-neutral-300 , change the text-color */}
+          <div className="font-sans font-extralight md:max-w-32 md:text-xs lg:text-base text-sm text-[#C1C2D3] z-10">
+            {description}
+          </div>
+          {/* add text-3xl max-w-96 , remove text-neutral-600 dark:text-neutral-300*/}
+          {/* remove mb-2 mt-2 */}
+          <div
+            className={`font-sans text-lg lg:text-3xl max-w-96 font-bold z-10`}
+          >
+            {title}
+          </div>
         </div>
-        <div className="font-sans font-normal text-neutral-600 text-xs dark:text-neutral-300">
-          {description}
+
+          {/* for the github 3d globe */}
+          {id === 2 && <GlobeDemo />}
+
+          
+    
+
         </div>
-      </div>
-    </div>
-    </div>
+        </div>
+
   );
 };
