cb5ce3013044101e5e29fa3d417d43ad0aa71c7d
Sachin Prabuditha <sachinprabuditha@gmail.com>
2024-06-18 02:43:19 +0530

    feat: Add SVG icons for HTML, CSS, JavaScript, Kotlin, and Android Studio
    This commit adds SVG icons for HTML, CSS, JavaScript, Kotlin, and Android Studio. These icons are used to represent these technologies in the application. The SVG files are added to the "public" directory.

--- components/ui/PinContainer.tsx
+++ components/ui/PinContainer.tsx
@@ -6,8 +6,8 @@ export function AnimatedPinDemo() {
   return (
     <div className="h-[40rem] w-full flex items-center justify-center ">
       <PinContainer
-        title="/ui.aceternity.com"
-        href="https://twitter.com/mannupaaji"
+        title="https://github.com/sachinprabuditha"
+        href="https://github.com/sachinprabuditha"
       >
         <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem] ">
           <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">
