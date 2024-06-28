72058f9351664dd7e94f616d5567320945dc4e07
Sachin Prabuditha <sachinprabuditha@gmail.com>
2024-06-29 00:58:06 +0530

    refactor: Update MagicButton component UI

--- components/ui/MagicButton.tsx
+++ components/ui/MagicButton.tsx
@@ -17,7 +17,7 @@ const MagicButton = ({
     <button className="relative inline-flex h-12 md:h-14 lg:h-16 overflow-hidden rounded-lg p-[1px] focus:outline-none" 
     onClick={handleClick}
     >
-  <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+  <span className="absolute inset-[-1000%] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
   <span
         className={`hover:bg-black-200 transition duration-300 inline-flex h-full w-full cursor-pointer items-center justify-center rounded-lg bg-slate-950 px-5 md:px-7 lg:px-10 text-xs md:text-sm lg:text-base font-medium text-white backdrop-blur-3xl gap-2 ${otherClasses}`}
       >
